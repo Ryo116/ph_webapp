1369091bd01efd4b5b59585d2cda613e316b19ee
Ryo116 <40268147+Ryo116@users.noreply.github.com>
2019-06-20 14:16:04 +0900

    Add files via upload

--- ave_hue.py
+++ ave_hue.py
@@ -17,22 +17,26 @@ img_hsv = cv2.cvtColor(img_scr, cv2.COLOR_BGR2HSV)
 height = img_hsv.shape[0]
 width = img_hsv.shape[1]
 
-pixels = []
+hue = []
 saturation = []
 
 for y in range(height):
     for x in range(width):
         if (img_hsv[y, x, 1] > 45 and 35 < img_hsv[y, x, 2]):
-            pixels.append(img_hsv[y, x, 0])
+            hue.append(img_hsv[y, x, 0])
             saturation.append(img_hsv[y, x, 1])
 
+# print("hue[] type is "+str(type(saturation)))
+# print(saturation)
+
+# グラフ表示設定
 plt.xlim(0, 180)
 plt.xticks([0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180])
-plt.hist(pixels, bins=24, range=(0, 180), align="mid", rwidth=1.0, color="blue")
+plt.hist(hue, bins=24, range=(0, 180), align="mid", rwidth=1.0, color="blue")
 
 bins = np.linspace(0, 180, 25)
 # print(bins)
-index = np.digitize(pixels, bins)
+index = np.digitize(hue, bins)
 c = Counter(index)
 # print(c)
 mode = c.most_common(1)
@@ -41,8 +45,8 @@ print(mode[0][0])
 saturation_average = mean(saturation)
 print('Saturaion Average:' + str(saturation_average))
 # 効果判定
-if mode[0][0] == 16 and int(saturation_average) > 65:
+if mode[0][0] == 16 and int(saturation_average) >= 65:
     print("Effective")
 else:
     print("No Effect")
-plt.show()
+# plt.show()
